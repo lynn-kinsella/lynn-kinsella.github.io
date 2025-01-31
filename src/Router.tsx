1bc3234eacc0a7aeefec69b8ab77957aec713643
Lynn Kinsella <lynnkinsella2000@gmail.com>
2025-01-30 21:44:32 -0500

    new create routes format

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,28 +1,35 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import { CafeCompassCase } from './pages/cases/CafeCompassCase';
 import { FlipFlossCase } from './pages/cases/FlipFlossCase';
 import { SvRaidsCase } from './pages/cases/SvRaidsCase';
 import { HomePage } from './pages/Home.page';
 
 const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <HomePage />,
-    },
-    {
-      path: '/case/cafe-compass',
-      element: <CafeCompassCase />,
-    },
-    {
-      path: '/case/sv-raids',
-      element: <SvRaidsCase />,
-    },
-    {
-      path: '/case/flip-floss',
-      element: <FlipFlossCase />,
-    },
-  ],
+    createRoutesFromElements(
+      <Route path="/" element={<HomePage/>}>
+        <Route path="/case/cafe-compass" element={<CafeCompassCase/>}/>
+        <Route path="/case/sv-raids" element={<SvRaidsCase/>}/>
+        <Route path="/case/flip-floss" element={<FlipFlossCase/>}/>
+      </Route>
+    ),
+  // [
+  //   {
+  //     path: '/',
+  //     element: <HomePage />,
+  //   },
+  //   {
+  //     path: '/case/cafe-compass',
+  //     element: <CafeCompassCase />,
+  //   },
+  //   {
+  //     path: '/case/sv-raids',
+  //     element: <SvRaidsCase />,
+  //   },
+  //   {
+  //     path: '/case/flip-floss',
+  //     element: <FlipFlossCase />,
+  //   },
+  // ],
   { basename: '/' }
 );
 
