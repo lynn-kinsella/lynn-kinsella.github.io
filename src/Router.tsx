9346b273869ba200b5dd268c7cbdd83b8c183464
Lynn Kinsella <lynnkinsella2000@gmail.com>
2025-01-30 22:05:30 -0500

    chatgpt public 404 suggestion

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,38 +1,44 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, HashRouter, Route, RouterProvider, Routes } from 'react-router-dom';
 import { CafeCompassCase } from './pages/cases/CafeCompassCase';
 import { FlipFlossCase } from './pages/cases/FlipFlossCase';
 import { SvRaidsCase } from './pages/cases/SvRaidsCase';
 import { HomePage } from './pages/Home.page';
 
+// createRoutesFromElements(
+const hashRouter = (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/case/cafe-compass" element={<CafeCompassCase />} />
+      <Route path="/case/sv-raids" element={<SvRaidsCase />} />
+      <Route path="/case/flip-floss" element={<FlipFlossCase />} />
+    </Routes>
+  </HashRouter>
+);
+
 const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<HomePage/>}>
-        <Route path="/case/cafe-compass" element={<CafeCompassCase/>}/>
-        <Route path="/case/sv-raids" element={<SvRaidsCase/>}/>
-        <Route path="/case/flip-floss" element={<FlipFlossCase/>}/>
-      </Route>
-    ),
-  // [
-  //   {
-  //     path: '/',
-  //     element: <HomePage />,
-  //   },
-  //   {
-  //     path: '/case/cafe-compass',
-  //     element: <CafeCompassCase />,
-  //   },
-  //   {
-  //     path: '/case/sv-raids',
-  //     element: <SvRaidsCase />,
-  //   },
-  //   {
-  //     path: '/case/flip-floss',
-  //     element: <FlipFlossCase />,
-  //   },
-  // ],
+  [
+    {
+      path: '/',
+      element: <HomePage />,
+    },
+    {
+      path: '/case/cafe-compass',
+      element: <CafeCompassCase />,
+    },
+    {
+      path: '/case/sv-raids',
+      element: <SvRaidsCase />,
+    },
+    {
+      path: '/case/flip-floss',
+      element: <FlipFlossCase />,
+    },
+  ],
   { basename: '/' }
 );
 
 export function Router() {
   return <RouterProvider router={router} />;
+  // return hashRouter
 }
