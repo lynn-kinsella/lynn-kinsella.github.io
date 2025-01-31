9baa2daa39d1b25753c212571fad7a543c7c045b
Lynn Kinsella <lynnkinsella2000@gmail.com>
2025-01-30 21:24:41 -0500

    update router to have base name

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,24 +4,27 @@ import { FlipFlossCase } from './pages/cases/FlipFlossCase';
 import { SvRaidsCase } from './pages/cases/SvRaidsCase';
 import { HomePage } from './pages/Home.page';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/case/cafe-compass',
-    element: <CafeCompassCase />,
-  },
-  {
-    path: '/case/sv-raids',
-    element: <SvRaidsCase />,
-  },
-  {
-    path: '/case/flip-floss',
-    element: <FlipFlossCase />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <HomePage />,
+    },
+    {
+      path: '/case/cafe-compass',
+      element: <CafeCompassCase />,
+    },
+    {
+      path: '/case/sv-raids',
+      element: <SvRaidsCase />,
+    },
+    {
+      path: '/case/flip-floss',
+      element: <FlipFlossCase />,
+    },
+  ],
+  { basename: '/' }
+);
 
 export function Router() {
   return <RouterProvider router={router} />;
